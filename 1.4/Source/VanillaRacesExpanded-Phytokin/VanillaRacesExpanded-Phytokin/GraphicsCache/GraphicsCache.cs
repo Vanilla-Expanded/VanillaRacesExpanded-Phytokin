a4fbee764c1f11c4132742fe5b0f0954eead6d91
SokyranTheDragon <exploder1531@gmail.com>
2024-02-01 23:39:29 +0100

    Cache solid Texture2D for the GeneGizmo_Resource bar patch
    Fixes the issue where the textures would get endlessly created and fill up RAM, similarly to the issue with VRE-Sanguophage mod.

--- 1.4/Source/VanillaRacesExpanded-Phytokin/VanillaRacesExpanded-Phytokin/GraphicsCache/GraphicsCache.cs
+++ 1.4/Source/VanillaRacesExpanded-Phytokin/VanillaRacesExpanded-Phytokin/GraphicsCache/GraphicsCache.cs
@@ -9,6 +9,9 @@ namespace VanillaRacesExpandedPhytokin
     {
 
         public static readonly Graphic graphicMaturePlant = (Graphic_Single)GraphicDatabase.Get<Graphic_Single>("Things/Plant/SaplingChild", ShaderDatabase.CutoutComplex, Vector2.one, Color.white);
-      
+
+        public static readonly Texture2D poluxAffinityBarTex = SolidColorMaterials.NewSolidColorTexture(new ColorInt(128, 108, 131).ToColor);
+        public static readonly Texture2D poluxAffinityBarHighlightTex = SolidColorMaterials.NewSolidColorTexture(new ColorInt(150, 127, 153).ToColor);
+
     }
 }
